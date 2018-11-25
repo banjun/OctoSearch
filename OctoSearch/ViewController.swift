9e2feeae35b66cea739c374efc86c27af85b32a2
banjun <banjun@gmail.com>
2018-11-25 21:59:15 +0900

    initial message

---
+++ OctoSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OctoSearch
+//
+//  Created by BAN Jun on 2018/11/25.
+//  Copyright © 2018 BAN Jun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
