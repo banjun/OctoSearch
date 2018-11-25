a1d63453d7253ff5725ff04e55856a99ca0a3045
banjun <banjun@gmail.com>
2018-11-25 22:02:19 +0900

    remove storyboard

--- OctoSearch/ViewController.swift
+++ OctoSearch/ViewController.swift
@@ -1,20 +1,12 @@
-//
-//  ViewController.swift
-//  OctoSearch
-//
-//  Created by BAN Jun on 2018/11/25.
-//  Copyright © 2018 BAN Jun. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+final class ViewController: UITableViewController {
+    init() {
+        super.init(style: .plain)
+        title = "Repositories"
     }
+    required init?(coder aDecoder: NSCoder) {fatalError()}
 
-
+    
 }
 
